9c922be7aa2e8b97a25a370dd02e9b6a8be64445
a24018304 <akim.amroune@etu.univ-amu.fr>
2025-01-31 16:56:10 +0100

    to string explicite et implicite (le commit juste avant + le privat vs public qui ne marche pas

--- src/Exercice2/TestCircle.java
+++ src/Exercice2/TestCircle.java
@@ -31,6 +31,11 @@ public class TestCircle {  // Save as "TestCircle.java"
       
       Circle c5 = new Circle(5.5);
       System.out.println(c5.toString());
+      
+      Circle c6 = new Circle(6.6);
+      System.out.println(c6.toString());  
+      System.out.println(c6);             
+      System.out.println("Operator '+' invokes toString() too: " + c6);  
 
    }
 }
